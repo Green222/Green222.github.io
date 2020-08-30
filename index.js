8bed636f4e78a3b688d8d541e2e7efba3383924d
Green <green.epita2016@gmail.com>
2020-08-31 01:31:31 +0200

    go

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
-	    if (xhr.readyState == XMLHttpRequest.DONE) {
-		            alert(xhr.responseText);
-		        }
+	if (xhr.readyState == XMLHttpRequest.DONE) {
+		alert(xhr.responseText);
+	}
 }
-xhr.open('GET', 'https://www.instacart.com/store', true);
+const json = {"visit_token":"e2cb2e49-2df8-4c6e-b67d-6bc1a6369e58"}
+xhr.open('POST', 'https://www.instacart.com/ahoy/visits', true);
+xhr.setRequestHeader('Content-type', 'application/json');
 xhr.withCredentials = true;
-xhr.send(null);
+xhr.send(JSON.stringify(json));
